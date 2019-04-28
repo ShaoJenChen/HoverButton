2eec8624cb873ac488f748d5caeab4adb969f840
ShaoJenChen <cash3310@outlook.com>
2019-04-28 12:43:55 +0800

    hover button change image by state

---
+++ HoverButton/HoverButton.swift
@@ -0,0 +1,62 @@
+//
+//  HoverButton.swift
+//  HoverButton
+//
+//  Created by 陳劭任 on 2019/4/28.
+//  Copyright © 2019 陳劭任. All rights reserved.
+//
+
+import Cocoa
+
+class HoverButton: NSButton {
+    
+    @IBInspectable var hoverImage: NSImage!
+    
+    required init?(coder: NSCoder) {
+        
+        super.init(coder: coder)
+        
+        self.addObserver(self, forKeyPath: "cell.state", options: .new, context: nil)
+        
+    }
+    
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        
+        if self.state == .on { swap(&self.image!, &self.hoverImage) }
+        
+    }
+    
+    override func updateTrackingAreas() {
+        
+        super.updateTrackingAreas()
+        
+        let options: NSTrackingArea.Options = [.inVisibleRect ,.mouseEnteredAndExited, .activeInKeyWindow]
+        
+        let trackingArea = NSTrackingArea(rect: NSRect.zero, options: options, owner: self, userInfo: nil)
+        
+        self.addTrackingArea(trackingArea)
+        
+    }
+    
+    override func mouseEntered(with event: NSEvent) {
+        
+        super.mouseEntered(with: event)
+        
+        guard self.isEnabled else { return }
+        
+        if self.state == .off { swap(&self.image!, &self.hoverImage) }
+        
+        
+    }
+    
+    override func mouseExited(with event: NSEvent) {
+        
+        super.mouseExited(with: event)
+        
+        guard self.isEnabled else { return }
+        
+        if self.state == .off { swap(&self.image!, &self.hoverImage) }
+        
+    }
+    
+}
--- HoverButton/ViewController.swift
+++ HoverButton/ViewController.swift
@@ -9,19 +9,49 @@
 import Cocoa
 
 class ViewController: NSViewController {
-
+    
+    @IBOutlet var btn1: HoverButton!
+    @IBOutlet var btn2: HoverButton!
+    @IBOutlet var btn3: HoverButton!
+    @IBOutlet var btn4: HoverButton!
+    @IBOutlet var btn5: HoverButton!
+    
+    var hoverButtons = [HoverButton]()
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        self.hoverButtons.append(contentsOf: [btn1, btn2, btn3, btn4, btn5])
+        
     }
 
     override var representedObject: Any? {
         didSet {
-        // Update the view, if already loaded.
         }
     }
 
 
+    private func clearOtherBtns(_ clickBtn: HoverButton) {
+        
+        for btn in self.hoverButtons {
+            
+            if btn == clickBtn { continue }
+            
+            btn.state = .off
+            
+            btn.isEnabled = true
+            
+        }
+        
+    }
+    
+    @IBAction func selectedHoverButton(_ sender: HoverButton) {
+        
+        self.clearOtherBtns(sender)
+        
+        sender.isEnabled = false
+        
+    }
 }
 
