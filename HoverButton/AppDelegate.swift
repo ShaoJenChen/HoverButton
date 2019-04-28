b69ea2ddd57a7a782e1506040319b0a1b6620b5f
ShaoJenChen <cash3310@outlook.com>
2019-04-28 10:27:26 +0800

    Initial Commit

---
+++ HoverButton/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  HoverButton
+//
+//  Created by 陳劭任 on 2019/4/28.
+//  Copyright © 2019 陳劭任. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ HoverButton/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  HoverButton
+//
+//  Created by 陳劭任 on 2019/4/28.
+//  Copyright © 2019 陳劭任. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
